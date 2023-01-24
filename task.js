b6eb58476a534c37c018478a8135a267da05dbdd
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-01-24 14:33:30 +0100

    first commit

---
+++ task.js
@@ -0,0 +1,87 @@
+//Basic JavaScript.
+
+
+//7. Understanding Case Sensitivity in Variables
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//8. Understanding Case Sensitivity in Variables
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+
+//9. Explore Differences Between the var and let Keywords
+var catName = "Oliver";
+var catSound = "Meow!";
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//10. Declare a Read-Only Variable with the const Keyword
+
+var fCC = "freeCodeCamp"; // Change this line
+var fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(fCC, fact); // Change this line
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//11. Add Two Numbers with JavaScript
+
+const sum = 10 + 10;
+
+//12. Subtract One Number from Another with JavaScript
+
+const difference = 45 - 33;
+
+//13. Multiply Two Numbers with JavaScript
+ const product = 8 * 10;
+
+ //14. Divide One Number by Another with JavaScript
+ const quotient = 66 / 33;
+
+ //15. Increment a Number with JavaScript
+ let myVar = 87;
+// Only change code below this line
+myVar++;
+
+//16. Decrement a Number with JavaScript
+let myDecrement = 11;
+// Only change code below this line
+myDecrement--;
+
+//17. Create Decimal Numbers with JavaScript
+const ourDecimal = 5.7;
+// Only change code below this line
+const myDecimal = 9.8;
+
+//18. Multiply Two Decimals with JavaScript
+const products = 2.0 * 2.5;
+
+//19. Divide One Decimal by Another with JavaScript
+const quotients = 4.4 / 2.0; // Change this line
+
+//20. Finding a Remainder in JavaScript
+const remainder = 11 % 3; //remainder gets the value 2
+
+
+
+
+
